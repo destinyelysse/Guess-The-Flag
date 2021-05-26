5c163f99c47dcc4293b8d2864dfd5f3aef2201ca
Destiny Serna <destinyeserna@gmail.com>
2021-05-25 22:40:07 -0700

    added accessibility improvements

--- Guess The Flag/GameView.swift
+++ Guess The Flag/GameView.swift
@@ -7,6 +7,20 @@ import SwiftUI
 
 struct GameView: View {
     
+    let labels = [
+        "estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
+        "france": "Flag with three vertical stripes of equal size. Left stripe blue, middle stripe white, right stripe red",
+        "germany": "Flag with three horizontal stripes of equal size. Top stripe black, middle stripe red, bottom stripe gold",
+        "ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
+        "italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
+        "nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
+        "poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
+        "russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
+        "spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
+        "uk": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
+        "us": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner"
+    ]
+    
     @State private var showingScore = false
     @State private var scoreTitle = ""
     
@@ -23,13 +37,17 @@ struct GameView: View {
             LinearGradient(gradient: Gradient(colors: [.blue, .purple]), startPoint: .top, endPoint: .bottom)
                 .edgesIgnoringSafeArea(.all)
             VStack(spacing: 30) {
-                Text("Guess the flag of...")
-                    .padding()
-                    .foregroundColor(.white)
-                Text(countries[correctAnswer].uppercased())
-                    .padding()
-                    .foregroundColor(.white)
-                    .font(.largeTitle)
+                VStack {
+                    Text("Guess the flag of...")
+                        .padding()
+                        .foregroundColor(.white)
+                    Text(countries[correctAnswer].uppercased())
+                        .padding()
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                }
+                .accessibilityElement(children: .ignore)
+                .accessibility(label: Text("Guess which of the 3 flags below is the flag of \(countries[correctAnswer])"))
                 VStack {
                     ForEach(0..<3) { number in
                         Button(action: {
@@ -38,6 +56,7 @@ struct GameView: View {
                             Image(self.countries[number])
                                 .renderingMode(.original)
                         }
+                        .accessibility(label: Text(self.labels[self.countries[number], default: "Unknown Flag"]))
                         .overlay(Rectangle().stroke(Color.white, lineWidth: 1))
                         .shadow(color: .black, radius: 3)
                         .rotation3DEffect(.degrees(Double(spinDegrees)), axis: (x:0, y:1, z:0))
