d1a48971f255c75554fbff6bd37371ddd2d86a00
Destiny Serna <destinyeserna@gmail.com>
2022-07-18 20:25:44 -0700

    Initial Commit

---
+++ guess-the-flag/guess_the_flagApp.swift
@@ -0,0 +1,17 @@
+//
+//  guess_the_flagApp.swift
+//  guess-the-flag
+//
+//  Created by Destiny Serna on 7/18/22.
+//
+
+import SwiftUI
+
+@main
+struct guess_the_flagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
