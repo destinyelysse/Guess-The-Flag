e8137fe637f89e79dc57eea4b8de4fb8dc07b068
Destiny Serna <destinyeserna@gmail.com>
2022-07-21 21:17:37 -0700

    Set up basic UI - added game prompt, text showing the flag to select, and flag buttons to pick from.

--- guess-the-flag/ContentView.swift
+++ guess-the-flag/ContentView.swift
@@ -9,10 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var alertIsShowing = false
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    
+    var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
-        Text("Howdy")
+        ZStack{
+            Color(hue: 0.65, saturation: 0.53, brightness: 1.0, opacity: 1.0).edgesIgnoringSafeArea(.all)
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                    Text(countries[correctAnswer])
+                }
+                ForEach(0 ..< 3){ number in
+                    Button(action: {
+                        // flag tapped
+                    }){
+                        Image(self.countries[number])
+                            .renderingMode(.original)
+                    }
+                }
+            }
+        }
     }
 }
 
